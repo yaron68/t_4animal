c5aae6c90825573c80f505a7bb85f813ce0d06db
yaron68 <yaron.chen68@gmail.com>
2024-06-14 09:47:38 +0300

    terra 1

--- ter_backup/4danimalsterraform/4danimal-terraform/variebles.tf
+++ ter_backup/4danimalsterraform/4danimal-terraform/variebles.tf
@@ -2,11 +2,4 @@ variable "region" {
     description = "value"
     default = "us-east-1"
 }
-variable "aws_access" {
-    description = "value"
-    default = ""
-}
-variable "aws_secret" {
-    description = "value"
-    default = ""
-}
+
