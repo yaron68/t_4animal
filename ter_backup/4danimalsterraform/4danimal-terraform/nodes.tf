69d58a360d3cf5c1e04491459c0f9124540f837f
yaron68 <128796562+yaron68@users.noreply.github.com>
2024-06-23 18:54:29 +0300

    Update nodes.tf

--- ter_backup/4danimalsterraform/4danimal-terraform/nodes.tf
+++ ter_backup/4danimalsterraform/4danimal-terraform/nodes.tf
@@ -44,14 +44,14 @@ resource "aws_eks_node_group" "private_nodes" {
     node_role_arn = aws_iam_role.nodes.arn
     subnet_ids = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id]
     capacity_type = "ON_DEMAND"
-    instance_types = ["t2.micro"]
+    instance_types = ["c5.xlarge"]
     scaling_config {
-        desired_size = 2
+        desired_size = 1
         max_size = 10
-        min_size = 2
+        min_size = 1
         }
     update_config {
-        max_unavailable = 2
+        max_unavailable = 1
     }
 
 labels = {
